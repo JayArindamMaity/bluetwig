d2f0254d2cac22af85aab37f056d308fd6db28a9
JayArindamMaity <maityjayarindam@gmail.com>
2025-10-22 10:45:46 +0000

    changes;

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -3,7 +3,16 @@ import './navbar.css'
 export default function Navbar () {
     return (
         <>
-            this is the code for navbar
+            <div className='nav-main'>
+                <div className='nav-ele'>
+                    <ul className='nav-lst'>
+                        <li>Home</li>
+                        <li>Leetcode</li>
+                        <li>CodeForces</li>
+                        <li>CodeChef</li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
